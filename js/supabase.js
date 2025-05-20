5d3821fd79d8b22fb3be8c4b4b98e6197badeb10
TrueSergey <80983006+TrueSergey@users.noreply.github.com>
2025-05-21 01:07:18 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
 // Инициализация клиента Supabase
 const supabaseUrl = 'https://mwbyuueswvspgvppwukh.supabase.co';
-const supabaseKey = 'your-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9-key';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13Ynl1dWVzd3ZzcGd2cHB3dWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3Nzc2OTMsImV4cCI6MjA2MzM1MzY5M30.enMt4OzkSeXC8psFJ0NJuecm0yY0a-yN9YjFmX6OKt0';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
