6f9a4eab0c69e3c14ce53799ae6e5d3e69fe57b1
TrueSergey <80983006+TrueSergey@users.noreply.github.com>
2025-05-21 13:34:16 +0000

    May 21, 2025, 4:34 PM

--- js/supabase.js
+++
@@ -1,5 +0,0 @@
-// Инициализация клиента Supabase
-const supabaseUrl = 'https://mwbyuueswvspgvppwukh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13Ynl1dWVzd3ZzcGd2cHB3dWtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3Nzc2OTMsImV4cCI6MjA2MzM1MzY5M30.enMt4OzkSeXC8psFJ0NJuecm0yY0a-yN9YjFmX6OKt0';
-// Правильно обращаемся к глобальному объекту supabase
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
